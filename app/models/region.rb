109365e8e561c44e43afb1c31c7087a8bf5fafa7
Mirv <reski.raptor@gmail.com>
2016-11-17 21:53:15 +0000

    Working with local var via partial

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,5 @@ class Region < ActiveRecord::Base
   belongs_to :continent
   has_many :domains 
   accepts_nested_attributes_for :domains, reject_if: :all_blank
+  accepts_nested_attributes_for :continent, reject_if: :all_blank
 end
